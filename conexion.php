0d7f4c0267495e44b23c3028dd7204bbe1612962
hooverlds2020 <hooverlds93@gmail.com>
2023-01-25 22:24:25 -0600

    first commit

---
+++ conexion.php
@@ -0,0 +1,11 @@
+<?php
+
+$conexion = new mysqli("localhost", "root", "", "microsoftware");
+
+// if($conexion){
+//     echo "Conexión exitosa!";
+// }else{
+//     echo "No hay conexión";
+// }
+
+?>
---
+++ guardar.php
@@ -0,0 +1,16 @@
+<?php
+
+include("conexion.php");
+
+$name = $_POST["name"];
+$imagen = addslashes(file_get_contents($_FILES["imagen"]["tmp_name"]));
+
+
+$query = "INSERT INTO producto(`name`, `imagen`)
+VALUES('$name', '$imagen')";
+
+$resultado = $conexion -> query($query);
+    if($resultado){
+        header("Location:consultar.php");
+    }
+?>
